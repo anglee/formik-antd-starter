eb38b1bb1e43665a1d9f6eac99ca78cf11a61f39
Ang Lee <ang.lee@gmail.com>
2019-12-02 08:07:20 -0500

    Play with LogRocket

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import LogRocket from 'logrocket';
 import App from './components/App/App';
 import store from './store';
 import './index.css';
+
+LogRocket.init('iclfld/test');
 
 ReactDOM.render(
   <Provider store={store}>
